87fea66d9aca3d620348e5990b6ff3c12e24f7e7
lijiaying@workstation <i@jiaying.li>
2015-11-22 01:02:16 +0800

    regular update

--- test/f3.cpp
+++ test/f3.cpp
@@ -18,5 +18,33 @@ int f3(int* a) {
 	return 0;
 }
 
-int main()
-{return 0;}
+int main(int argc, char** argv)
+{
+	if (argc >= 3) {
+		minv = atoi(argv[1]);
+		maxv = atoi(argv[2]);
+	}
+
+	States global_states_sets[4];
+	States* gsets = &global_states_sets[1];
+
+	if (register_program(conj, "conj") == false) {
+		return -1;
+	}
+
+	IIF_svm_learn isl(gsets, target_program);
+	if (isl.learn() == 0) {
+		std::cout << "END.\n";
+		return 0;
+	}
+
+	std::cout << "TRY SVM-I method again...\n";
+	IIF_svm_i_learn isil(gsets, target_program);
+	if (isil.learn() == 0) {
+		std::cout << "END.\n";
+		return 0;
+	}
+
+	std::cout << "TRY other method again...\n";
+	return 0;
+}
