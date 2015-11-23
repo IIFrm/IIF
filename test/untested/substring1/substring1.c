ece0c2816148c23b863f8878b4a188b32fb77379
lijiaying@workstation <i@jiaying.li>
2015-11-23 17:24:11 +0800

    untested

--- test/untested/substring1/substring1.c
+++ test/untested/substring1/substring1.c
@@ -1,27 +1,27 @@
 void main () {
-int i, j;
-int from;
-int to;
-int k;
+	int i, j;
+	int from;
+	int to;
+	int k;
 
-if (!(k >= 0)) return;
-if (!(k <= 100)) return;
+	if (!(k >= 0)) return;
+	if (!(k <= 100)) return;
 
-if (!(from >= 0)) return;
-if (!(from <= k)) return;
+	if (!(from >= 0)) return;
+	if (!(from <= k)) return;
 
-i = from;
-j = 0;
+	i = from;
+	j = 0;
 
-while (i < k) {
-i++;
-j++;
-}
+	while (i < k) {
+		i++;
+		j++;
+	}
 
-if (j >= 101)
-  goto ERROR;
+	if (j >= 101)
+		goto ERROR;
 
-  return;
+	return;
 
 ERROR:;
 
