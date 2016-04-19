e1ce4012daaeca3fef83fb1e7abd2b870e7cb8d8
lijiaying@workstation <i@jiaying.li>
2016-04-20 05:16:59 +0800

    deal with the issue of the involved bit integer, which is due to the bitvector encoding.

---
+++ test/interproc_test3.cpp
@@ -0,0 +1,27 @@
+#include "iif.h"
+#include <iostream>
+using namespace iif;
+
+int loopFunction(int a[]) {
+int i = a[0];
+int j = a[1];
+int k = a[2];
+
+iif_assume(i<j&&k>0);
+while(i<j)
+{
+recordi(i, j, k);k=k+1;i=i+1;
+}
+recordi(i, j, k);
+iif_assert(k>j-i);
+return 0;
+}
+
+int main(int argc, char** argv)
+ {
+iifContext context("../tmp/interproc_test3.var", loopFunction, "loopFunction", "../tmp/interproc_test3.ds");
+context.addLearner("linear", "../tmp/interproc_test3.cnt");
+context.addLearner("poly", "../tmp/interproc_test3.cnt");
+context.addLearner("conjunctive", "../tmp/interproc_test3.cnt");
+return context.learn("../tmp/interproc_test3");
+}
